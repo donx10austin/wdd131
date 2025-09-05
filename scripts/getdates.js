e7911da522d6eba94037dab448d13e9c7989f619
donx10austin <aaustin1@byupathway.edu>
2025-09-05 18:09:26 +0100

    moderate

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,17 +1,22 @@
 
+
+
 const currentYear = new Date().getFullYear();
-document.getElementById('currentyear').textContent = currentYear;
 
-const lastModifiedDate = new Date(document.lastModified);
-const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit'
-};
+const yearSpan = document.getElementById('currentyear');
+if (yearSpan) {
+    yearSpan.textContent = currentYear;
+}
 
-const formattedDate = lastModifiedDate.toLocaleString('en-US', options);
 
-document.getElementById('lastModified').textContent = `Last Modified: ${formattedDate}`;
+const lastModified = document.lastModified;
+
+const lastModifiedPara = document.getElementById('lastModified');
+if (lastModifiedPara) {
+    lastModifiedPara.textContent = `Last Modified: ${lastModified}`;
+}
+
+
+
+
+

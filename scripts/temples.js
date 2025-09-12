bd6fdd8fc040cb779bb397d75b08b259116b76db
donx10austin <aaustin1@byupathway.edu>
2025-09-12 21:47:34 +0100

    checking now

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,11 +1,4 @@
-const menuButton = document.getElementById('menuButton');
-const mainNav = document.getElementById('mainNav');
-
-menuButton.addEventListener('click', () => {
-  mainNav.classList.toggle('show');
-  const expanded = menuButton.getAttribute('aria-expanded') === 'true';
-  menuButton.setAttribute('aria-expanded', !expanded);
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('copyrightYear').textContent = new Date().getFullYear();
+  document.getElementById('lastModified').textContent = document.lastModified;
 });
-
-document.getElementById('copyrightYear').textContent = new Date().getFullYear();
-document.getElementById('lastModified').textContent = document.lastModified;
